391fbc08de5059f0a46d9a798c20a32c2b368385
Tobias Kirchner <kirchner.to@gmail.com>
2024-04-24 18:33:30 +0200

    Add Attribution to Core Web Vitals

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    experimental: {
+        webVitalsAttribution: ['CLS', 'LCP', 'FID', 'TTFB', 'INP']
+      }
+
+};
 
 export default nextConfig;
